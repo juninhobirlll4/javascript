5f383fb405eea617bfa28d6979afe56094d03be8
Júnior <joseluizdesouzajuniorjl@gmail.com>
2025-04-26 14:18:58 -0300

    Operadores Aritméticos

---
+++ classroom/javascript-basico/ex005/script.js
@@ -0,0 +1,8 @@
+//Operadores aritmeticos
+
+console.log("A SOMA É IGUAL A: ", + 12 + 2)
+console.log("SUBSTRAÇÃO: ", 12 - 8)
+console.log("MULTIPLICAÇÃO: ", 3 * 3.5)
+console.log("DIVISÃO: ", 12 / 2)
+console.log("RESTO DA DIVISÃO: ", 13 % 2)
+console.log("EXPONENCIAL:", 3**3) //OU SEJA 3 ELEVADO PELO NUMERO DA FRENTE QUE É "3"
