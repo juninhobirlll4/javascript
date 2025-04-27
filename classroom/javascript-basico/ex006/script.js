1c5ee36ad3e85c5ea663d4007797308171592ccb
Júnior <joseluizdesouzajuniorjl@gmail.com>
2025-04-27 15:25:43 -0300

    Incremento e decremento, ordem de precedência.

---
+++ classroom/javascript-basico/ex006/script.js
@@ -0,0 +1,35 @@
+let number = 10
+
+
+// Incremento
+//number = number + 1
+number++
+
+//Incrementa apos (por isso não mostra no console)
+console.log(number++,)
+console.log(number)
+
+//Incrementa antes
+console.log(++number)
+
+//Decremento
+console.log("Decremento após: ", number--)
+console.log(number--)
+
+//Decremento antes
+
+console.log(--number)
+
+//Adicionando mais 10 no number
+number += 10
+console.log(number)
+
+//Retirando -2 no number
+number-= 2
+console.log(number)
+
+/*
+A mesma coisa de:
+number = number + 20, ou
+number = number - 20
+*/
