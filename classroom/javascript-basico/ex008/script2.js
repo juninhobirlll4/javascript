2ddb3187fb882544059ce0630976cef48534645b
Júnior <joseluizdesouzajuniorjl@gmail.com>
2025-04-27 21:06:11 -0300

    Operadores logicos.

---
+++ classroom/javascript-basico/ex008/script2.js
@@ -0,0 +1,24 @@
+let balance = 500
+let payment = 120
+
+// > Maior que
+
+console.log(balance>payment)
+
+// < Menor que
+
+console.log(balance<payment)
+
+//Maior ou igual >=
+
+console.log(`
+    ####
+    ####
+    ####`)
+balance = 120
+console.log(balance>=payment)
+
+//Menor ou igual <=
+
+balance = 700
+console.log(balance<=payment)
---
+++ classroom/javascript-basico/ex008/script3.js
@@ -0,0 +1,25 @@
+let value
+
+// Operador atribuição
+value = 3
+console.log(value)
+
+// Incremento
+value += 2
+console.log(value)
+
+//Decremento
+value -= 2
+console.log(value)
+
+// Multiplicar
+value *= 3
+console.log(value)
+
+// Divisão
+value /= 2
+console.log(value)
+
+// Potencia
+value **= 2
+console.log(value)
