d1b67b1ab999d66306d07d06d4c636ddf4ffcffa
Júnior <joseluizdesouzajuniorjl@gmail.com>
2025-05-04 14:17:14 -0300

    Manipulando conteudo

---
+++ classroom/javascript-basico/ex013/script3.js
@@ -0,0 +1,8 @@
+const guest = document.querySelector("#guest-1")
+//console.log(guest.textContent) //PERMITE VISUALIZAR O CONTEUDO EM TEXTO
+
+//guest.textContent = "João da Silva" //FICAR ATENTO ELE SUBSTITUI TUDO QUE ESTÁ DENTRO COMO TEXTO
+
+console.log(guest.textContent) // MOSTRA O CONTEUDO VISIVEL E OCULTO
+console.log(guest.innerText) // ESSE MOSTRA O TEXTO DA CLASSE, ID E ETC.
+console.log(guest.innerHTML) // ESSE MOSTRA O HTML COMO TEXTO
